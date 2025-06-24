5d4162962dfbad91d1c979c64195da4e6380c90d
Eshwar K S <eshwarks1234@gmail.com>
2025-06-25 00:11:36 +0530

    Initial Commit

---
+++ crud-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const title = "React CRUD App";
+  const likes = 10;
+  // const person = {
+  //   name: 'John',
+  //   age: 30,
+  // };
+  const link = "https://www.amazon.com";
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times </p>
+
+        {/* <p>{person}</p>
+         Cannot display object/boolean directly in JSX */}
+
+        <p>{55}</p>
+        <p>{`hello ninjas`} </p>
+        <p>{[1, 2, 4, 3, 2]} </p>
+        <p>{Math.random() * 10} </p>
+
+        <a href={link}>Amazon Site</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
