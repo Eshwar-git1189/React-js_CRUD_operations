c564374c1afcc2f13b2c0c82192afaf5847325f4
Eshwar K S <eshwarks1234@gmail.com>
2025-06-25 13:00:31 +0530

    Added Components and styles

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,30 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+
+// function App() {
+//   const title = "React CRUD App";
+//   const likes = 10;
+//   // const person = {
+//   //   name: 'John',
+//   //   age: 30,
+//   // };
+//   const link = "https://www.amazon.com";
+
+//   return (
+//     <div className="App">
+//       <div className="content">
+//         <h1>{title}</h1>
+//         <p>Liked {likes} times </p>
+
+//         {/* <p>{person}</p>
+//          Cannot display object/boolean directly in JSX */}
+
+//         <p>{55}</p>
+//         <p>{`hello ninjas`} </p>
+//         <p>{[1, 2, 4, 3, 2]} </p>
+//         <p>{Math.random() * 10} </p>
+
+//         <a href={link}>Amazon Site</a>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
 
 function App() {
-  const title = "React CRUD App";
-  const likes = 10;
-  // const person = {
-  //   name: 'John',
-  //   age: 30,
-  // };
-  const link = "https://www.amazon.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times </p>
-
-        {/* <p>{person}</p>
-         Cannot display object/boolean directly in JSX */}
-
-        <p>{55}</p>
-        <p>{`hello ninjas`} </p>
-        <p>{[1, 2, 4, 3, 2]} </p>
-        <p>{Math.random() * 10} </p>
-
-        <a href={link}>Amazon Site</a>
+        <Home />
       </div>
     </div>
   );
