096715f840684761f42cf0ac5d9bae3e178910c1
Eshwar K S <eshwarks1234@gmail.com>
2025-06-25 17:44:10 +0530

    On Click Operations

--- crud-app/src/Home.js
+++ crud-app/src/Home.js
@@ -1,9 +1,25 @@
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
-    );
-}
+  const handleClick = (e) => {
+    console.log(`Hello Ninjas`, e);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log(`Hello  ${name}`, e.target);
+  };
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click </button>
+      {/* <button onClick={handleClickAgain("Eshwar")}>Click me again</button>
+      This makes function to get invoked before Click */}
+      <button
+        onClick={(e) => {
+          handleClickAgain(`Eshwar`, e);
+        }}
+      >
+        Click me Again
+      </button>
+    </div>
+  );
+};
 
 export default Home;
