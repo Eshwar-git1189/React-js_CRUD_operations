6c9e4f69d733057f57715c42138e157353d1aa9f
Eshwar K S <eshwarks1234@gmail.com>
2025-06-28 19:47:11 +0530

    Functions as props(Delete an Item)

--- crud-app/src/BlogList.js
+++ crud-app/src/BlogList.js
@@ -1,8 +1,9 @@
-//  const BlogList = ({blogs,title}) => { (We can destructure props object and instead we can directly use blogs,title 
+//  const BlogList = ({blogs,title}) => { (We can destructure props object and instead we can directly use blogs,title
 const BlogList = (props) => {
   const blogs = props.blogs;
   const title = props.title;
-
+  const handleDelete = props.handleDelete;
+// Here we should not define handleDelete function 
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -10,6 +11,7 @@ const BlogList = (props) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written By {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- crud-app/src/Home.js
+++ crud-app/src/Home.js
@@ -27,36 +27,59 @@
 import { useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-//   // let name = 'Mario';
-//   // const handleClick = () => {
-//   //   name = 'luigi';
-//   //   console.log(name);
-    
-//   // };
-//   const [name, setName] = useState('Mario');//Data type of values used inside the useState doesn't matter here. It can be Object,number,boolean,string etc.. 
-//   const [age,setAge] = useState(25);
-//   const handleClick = () => {
-//     setName('Luigi');
-//     setAge(30);
-//   }
-  const [blogs, setBlogs]= useState([
-    {title: 'My new website', body: 'Lorem ipsum dolor sit amet', author: 'Mario', id: 1},
-    {title: 'Welcome party!', body: 'Lorem ipsum dolor sit amet', author: 'Yoshi', id: 2},
-    {title: 'Web dev top tips', body: 'Lorem ipsum dolor sit amet',author: 'Mario', id: 3}
+  //   // let name = 'Mario';
+  //   // const handleClick = () => {
+  //   //   name = 'luigi';
+  //   //   console.log(name);
+
+  //   // };
+  //   const [name, setName] = useState('Mario');//Data type of values used inside the useState doesn't matter here. It can be Object,number,boolean,string etc..
+  //   const [age,setAge] = useState(25);
+  //   const handleClick = () => {
+  //     setName('Luigi');
+  //     setAge(30);
+  //   }
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "Lorem ipsum dolor sit amet",
+      author: "Mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "Lorem ipsum dolor sit amet",
+      author: "Yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "Lorem ipsum dolor sit amet",
+      author: "Mario",
+      id: 3,
+    },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+    console.log(`Deleted blog with id: ${id}`);
+  };
   return (
     <div className="home">
       {/* <h2>Home Page</h2>
       <p>{name} is {age} years old.</p>
       <button onClick={handleClick}>Click </button> */}
-      
-       {/* Using map to iterate over the array of objects */}
-      <BlogList blogs={blogs} title = "All Blogs!"/>
-      {/* Here we use props to pass data from parent component to child component. */}
 
-      
+      {/* Using map to iterate over the array of objects */}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {/* Here we use props to pass data from parent component to child component. */}
+      {/* <BlogList
+        blogs={blogs.filter((blog) => blog.author === "Mario")}
+        title="Mario's blogs"
+      /> */}
     </div>
   );
-}
+};
 
 export default Home;
