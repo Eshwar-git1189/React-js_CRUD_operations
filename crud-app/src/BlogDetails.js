fd93ee73445ea32994e6964fcecb712598e6b801
Eshwar K S <eshwarks1234@gmail.com>
2025-06-30 15:25:02 +0530

    Reusing a custom hook

--- crud-app/src/BlogDetails.js
+++ crud-app/src/BlogDetails.js
@@ -1,11 +1,29 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-const {id} = useParams(); // Grab parameters from routes
-    return (
-        <div className="blog-details">
-            <h2>Blog Details-{id}</h2>
-        </div>
-    );
-}
+  const { id } = useParams(); // Grab parameters from routes
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  console.log("ID from useParams:", id);
+  console.log("Full fetch URL:", "http://localhost:8000/blogs/" + id);
+  console.log("Fetched blog:", blog);
+
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error} </div>}
+      {blog && (
+        <article>
+          <h2>{blog.title} </h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body} </div>
+        </article>
+      )}
+    </div>
+  );
+};
 export default BlogDetails;
